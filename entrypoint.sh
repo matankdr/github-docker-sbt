af203642d5ebaed89343e4fc5188529d22749e6d
kobi-lemberg <kobi.lemberg@pipl.com>
2020-03-16 16:16:14 +0200

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,12 @@
 
 sbt version
 set -x
-${@}
+
+DOCKER_USERNAME=$1
+DOCKER_PASSWORD=$2
+DOCKER_REGISTRY=$3
+
+echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin ${DOCKER_REGISTRY}
+
+$4
 
