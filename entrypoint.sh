d23bacd1e86e3ab9ceccbb41be983c0851376c03
kobi-lemberg <kobi.lemberg@pipl.com>
2020-03-17 11:25:54 +0200

    Make docker login to be configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-sbt version
 set -x
+COMMAND=""
 
-DOCKER_USERNAME=$1
-DOCKER_PASSWORD=$2
-DOCKER_REGISTRY=$3
+if [[ -z ${1} || -z ${2} || -z ${3} ]]; then
+  echo "One or more variables are not defined, will run command"
+  COMMAND = $1
+else
+  DOCKER_USERNAME=$1
+  DOCKER_PASSWORD=$2
+  DOCKER_REGISTRY=$3
+  COMMAND=$4
+  echo "Running docker login into ${DOCKER_REGISTRY}"
 
-echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin ${DOCKER_REGISTRY}
+  echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin ${DOCKER_REGISTRY}
+fi
 
-$4
+echo "Running command"
+${COMMAND}
 
