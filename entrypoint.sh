0e84d0973ae04aa8cbb9c516a1a5de495f2fb5d5
kobi-lemberg <kobi.lemberg@pipl.com>
2020-03-23 13:12:07 +0200

    Fix creds

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 set -x
 COMMAND=$1
-IVY_CONF=$2
+IVY_CONF=$(echo $2 | base64 -d)
+echo "Printing IVY conf"
+echo $IVY_CONF
 if [[ -z "${3}" || -z "${4}" || -z "${5}" ]]; then
   echo "One or more variables are not defined, will only run command"
 else
@@ -16,10 +18,11 @@ fi
 mkdir ~/.ivy2/ || echo "Ivy folder is exists"
 mkdir /root/.ivy2/ || echo "Ivy folder is exists"
 
-echo ${IVY_CONF} > ~/.ivy2/credentials || echo "Couldn't write ivy into ~/.ivy2/credentials"
+echo ${IVY_CONF} > ${HOME}/.ivy2/credentials || echo "Couldn't write ivy into ~/.ivy2/credentials"
 echo ${IVY_CONF} > /root/.ivy2/credentials || sudo echo ${IVY_CONF} > /root/.ivy2/credentials || echo "Couldn't write ivy into /root/.ivy2/credentials"
-echo $(cat ~/.ivy2/credentials) || echo "~/.ivy2/credentials not found"
+echo $(cat ${HOME}/.ivy2/credentials) || echo "~/.ivy2/credentials not found"
 echo $(cat /root/.ivy2/credentials) || echo "/root/.ivy2/credentials"
 echo "Running command"
 ${COMMAND}
 
+11.0.5-1.3.8-DOCKER-IVY-CREDS8
