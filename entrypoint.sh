56e0bab493b04895751b87ea0b053b9e802a8779
Roman Nazarenko <me@jtalk.me>
2020-04-22 19:32:02 +0100

    Support a working directory parameter.
    Auto-cd into a project subdirectory

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,17 @@ git config --global user.name ${GITHUB_USERNAME}
 git config --global user.password ${GITHUB_PASSWORD}
 
 COMMAND=$1
-if [[ -z "${2}" || -z "${3}" || -z "${4}" ]]; then
+WORKING_DIR=$2
+if [ -z "${3}" ] || [ -z "${4}" ] || [ -z "${5}" ]; then
   echo "One or more variables are not defined, will only run command"
 else
-  DOCKER_USERNAME=$2
-  DOCKER_PASSWORD=$3
-  DOCKER_REGISTRY=$4
+  DOCKER_USERNAME=$3
+  DOCKER_PASSWORD=$4
+  DOCKER_REGISTRY=$5
   echo "Running docker login into ${DOCKER_REGISTRY}"
   echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin ${DOCKER_REGISTRY}
 fi
 
-echo "Running command"
+echo "Running command in ${WORKING_DIR}"
+cd "$WORKING_DIR" || { echo "Cannot change into directory ${WORKING_DIR}"; exit 1; }
 ${COMMAND}
